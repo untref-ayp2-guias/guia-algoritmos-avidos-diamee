c739cfdabdf033b5aa0bb12141900df1e9494c27
Diamela Riccio <diamelariccio24@hotmail.com>
2025-06-20 17:21:40 -0300

    ej1

--- ejercicios/ejercicio1.go
+++ ejercicios/ejercicio1.go
@@ -1,5 +1,7 @@
 package ejercicios
 
+import "fmt"
+
 type Actividad struct {
 	Nombre string
 	Inicio int
@@ -7,5 +9,21 @@ type Actividad struct {
 }
 
 func SelectorRecursivo(actividades []Actividad) []Actividad {
-	panic("No implementado")
+	actividadesElegidas := make([]Actividad, 0)
+	actividadesElegidas = append(actividadesElegidas, actividades[0])
+	aux(actividades[1:], &actividadesElegidas)
+	return actividadesElegidas
+}
+
+func aux(actividad []Actividad, actSeleccionada *[]Actividad) {
+
+	if len(actividad) == 0 {
+		return
+	}
+	ultima := (*actSeleccionada)[len(*actSeleccionada)-1]
+	if actividad[0].Inicio >= ultima.Fin {
+		fmt.Println("entro")
+		*actSeleccionada = append(*actSeleccionada, actividad[0])
+	}
+	aux(actividad[1:], actSeleccionada)
 }
